f1eb14be118108848ff6070023d5260a5a6c9e5b
KevinCrz <kevingcruz99@gmail.com>
2024-04-23 13:50:21 -0300

    Atualizando e corrigindo atividade 11

--- atividade-11/main.js
+++ atividade-11/main.js
@@ -1,229 +1,256 @@
-
-//FUNÇÃO PUSH 
-
-const meuArrayEl = document.getElementById("arrayDisplay")
+const meuArrayEl = document.getElementById("arrayDisplay");
 let array = [];
 
+// PUSH
+
 function push(array, ...items) {
-    for (let item of items) {
-        array[array.length] = item;
+    for (let i = 0; i < items.length; i++) {
+        array[array.length] = items[i];
     }
     return array.length;
 }
 
 function IncluiPush() {
-
     let meuValor = document.getElementById("meuInput").value;
-    
-    array.push(meuValor.trim());
-    
-    if(meuValor)
-    {
-        if(!meuArrayEl.innerText)
-        {
+
+    push(array, meuValor.trim());
+
+    if (meuValor) {
+        if (!meuArrayEl.innerText) {
             meuArrayEl.innerText = array.join(" ");
-        }else{
+        } else {
             meuArrayEl.innerText = array.join(", ");
         }
         document.getElementById("meuInput").value = "";
-
-    }else{
-
     }
-
 }
 
-//FUNÇÃO POP
+// POP
 
 function pop(array) {
-    if (array.length === 0) {
-        return undefined;
-    }
-    return array.pop();
+    const value = array[array.length - 1];
+    array.length -= 1;
+    return value;
 }
 
-function execPop() {
+const arr = [''];
 
+function execPop() {
     let removedItem = pop(array);
 
     if (meuArrayEl.innerText) {
-
-        meuArrayEl.innerText = array; 
-        alert("Item removido: " + removedItem); 
-
+        meuArrayEl.innerText = array;
+        alert("Item removido: " + removedItem);
     } else {
-
         alert("O array está vazio!");
     }
 }
 
-//FUNÇÃO SHIFT
+// SHIFT
 
 function shift(array) {
-    if (array.length === 0) {
-        return undefined;
+    const value = array[0];
+
+    for (let i = 0; i < array.length - 1; i++) {
+        array[i] = array[i + 1];
     }
-    return array.shift();
+    
+    array.length -= 1;
+
+    return value;
 }
 
 function execShift() {
-
     let removedItem = shift(array);
 
     if (meuArrayEl.innerText) {
-
-        meuArrayEl.innerText = array; 
-        alert("Item removido: " + removedItem); 
-
+        meuArrayEl.innerText = array;
+        alert("Item removido: " + removedItem);
     } else {
-
         alert("O array está vazio!");
     }
 }
 
-//FUNÇÃO UNSHIFT
+// UNSHIFT
 
 function unshift(array, ...items) {
-    for (let i = items.length - 1; i >= 0; i--) {
-
-        array.unshift(items[i]);
+    for (let i = array.length - 1; i >= 0; i--) {
+        array[i + items.length] = array[i];
+    }
+    for (let i = 0; i < items.length; i++) {
+        array[i] = items[i];
     }
     return array.length;
 }
 
 function execUnShift() {
-
-    if(meuArrayEl.innerText)
-    {
-        let inputValue = document.getElementById("inputUnshift").value; 
-
+    if (meuArrayEl.innerText) {
+        let inputValue = document.getElementById("inputUnshift").value;
         unshift(array, inputValue.trim());
-    
-        meuArrayEl.innerText = array
+        meuArrayEl.innerText = array;
         document.getElementById("inputUnshift").value = "";
     } else {
-
-        alert("O array está vazio!"); 
+        alert("O array está vazio!");
         document.getElementById("inputUnshift").value = "";
     }
-
 }
 
-//FUNÇÃO SLICE
+// SLICE
 
-function slice(array, start, end) {
-    return array.slice(start, end);
+function slice(array, indexStart, indexEnd = -1) {
+    const newArray = [];
+
+    indexEnd = indexEnd < 0 ? array.length : indexEnd;
+
+    for (let i = indexStart; i <= indexEnd; i++) {
+        newArray[newArray.length] = array[i];
+    }
+
+    return newArray;
 }
-
 
 function execSlice() {
-
     let startValue = parseInt(document.getElementById("initSlice").value);
-    let endValue = parseInt(document.getElementById("endSlice").value); 
+    let endValue = parseInt(document.getElementById("endSlice").value);
 
-    let subarray = slice(array, startValue, endValue); 
-    document.getElementById("subarrayDisplay").innerText = subarray; 
+    let subarray = slice(array, startValue, endValue);
+    document.getElementById("subarrayDisplay").innerText = subarray;
 }
 
-//FUNÇÃO SPLICE
+// SPLICE
 
-function splice(array, start, deleteCount, ...items) {
-    array.splice(start, deleteCount, ...items);
-    return array;
+function splice(array, indexStart, deleteCount, ...items) {
+
+    const newArray = [];
+    const arrayToDelete = [];
+
+    for (let i = indexStart; i < array.length; i++) {
+        if (i >= indexStart && i < indexStart + deleteCount) {
+            arrayToDelete.push(array[i]);
+        } else {
+            newArray.push(array[i]);
+        }
+    }
+
+    for (let i = 0; i < newArray.length; i++) {
+        array[i] = newArray[i];
+    }
+
+    array.length = newArray.length;
+
+    for (let i = array.length - 1; i >= indexStart; i--) {
+        array[i + 1 + items.length] = array[i];
+    }
+
+    for (let i = 0; i < items.length; i++) {
+        array[indexStart + i] = items[i];
+    }
+
+    return arrayToDelete;
 }
 
 function execSplice() {
-
-
-    if(meuArrayEl.innerText)
-    {
+    if (meuArrayEl.innerText) {
 
         let inicioSplice = parseInt(document.getElementById("initSplice").value);
         let quantidadeSplice = parseInt(document.getElementById("qntSplice").value);
-        
         let novosItens = document.getElementById("newSplice").value.split(",");
-    
-        let newArray = splice(array, inicioSplice, quantidadeSplice, ...novosItens); 
+        
+        let newArray = splice(array, inicioSplice, quantidadeSplice, ...novosItens);
         document.getElementById("subarrayDisplay2").innerText = newArray;
-
+    
     } else {
 
-        alert("O array está vazio!"); 
+        alert("O array está vazio!");
         document.getElementById("inputUnshift").value = "";
     }
-
-
 }
 
-//FUNÇÃO FOREACH
+// FOREACH
 
 function forEach(array, callback, value) {
     for (let i = 0; i < array.length; i++) {
-        callback(array[i], i, array, value);
+        
+        if (value !== undefined) {
+            callback(array[i], i, array, value);
+        } else {
+            callback(array[i], i, array);
+        }
     }
 }
 
 function addValueToElement(element, index, array, value) {
-    array[index] = element + value;
+    
+    if (value !== undefined) {
+        array[index] = element + value;
+    } else {
+        array[index] = element;
+    }
 }
 
 function forEachArray() {
 
-    if(meuArrayEl.innerText)
-    {
-        let value = document.getElementById("novoElemento").value;
+    let novoElementoEl = document.getElementById("novoElemento");
+    let inputUnshiftEl = document.getElementById("inputUnshift");
 
-        forEach(array, addValueToElement, value); 
-    
+    if (meuArrayEl.innerText) {
+
+        let value = novoElementoEl.value;
+        forEach(array, addValueToElement, value);
+        
         document.getElementById("arrayForEach").innerText = array;
-        document.getElementById("novoElemento").value = "";
+        novoElementoEl.value = "";
 
     } else {
 
-        alert("O array está vazio!"); 
-        document.getElementById("inputUnshift").value = "";
+        alert("O array está vazio!");
+        inputUnshiftEl.value = "";
     }
-
 }
 
-//FUNÇÃO MAP
+// MAP
 
-function map(array, callback, value) {
-
-    return array.map(function(element) {
-        return callback(element, value);
-    });
+function map(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray[i] = callback(array[i], i, array);
+    }
+    return newArray;
 }
 
 function addValueToElement(element, value) {
-    return element + value; 
+    return element + value;
 }
 
 function execMap() {
-
-    if(meuArrayEl.innerText)
-    {
-        let value = document.getElementById("mapInput").value;
     
-        let newArray = map(array, addValueToElement, parseInt(value));
+    let mapInputEl = document.getElementById("mapInput");
+    let inputUnshiftEl = document.getElementById("inputUnshift");
+
+    if (meuArrayEl.innerText) {
         
-        document.getElementById("arrayMap").innerText = newArray; 
-        document.getElementById("mapInput").value = ""; 
+        let value = mapInputEl.value;
+        let newArray = map(array, (element) => addValueToElement(element, parseInt(value)));
+        
+        document.getElementById("arrayMap").innerText = newArray;
+        mapInputEl.value = "";
 
     } else {
-
-        alert("O array está vazio!"); 
-        document.getElementById("inputUnshift").value = "";
+        alert("O array está vazio!");
+        inputUnshiftEl.value = "";
     }
-    
 }
 
-//FUNÇÃO FILTER
+// FILTER
 
-function filter(array, callback, value) {
-    return array.filter(function(element) {
-        return callback(element, value);
-    });
+function filter(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 }
 
 function filterElement(element, value) {
@@ -231,55 +258,53 @@ function filterElement(element, value) {
 }
 
 function execFilter() {
-
-    if(meuArrayEl.innerText)
-    {
-
-        let value = document.getElementById("filterInput").value; 
     
-        let filteredArray = filter(array, filterElement, value); 
+    let filterInputEl = document.getElementById("filterInput");
+    let inputUnshiftEl = document.getElementById("inputUnshift");
+
+    if (meuArrayEl.innerText) {
+        
+        let value = filterInputEl.value;
+        let filteredArray = filter(array, (element) => filterElement(element, value));
         
         document.getElementById("arrayFilter").innerText = filteredArray;
 
     } else {
-
-        alert("O array está vazio!"); 
-        document.getElementById("inputUnshift").value = "";
+        alert("O array está vazio!");
+        inputUnshiftEl.value = "";
     }
-
-
 }
 
-//FUNÇÃO REDUCE
+// REDUCE
 
 function reduce(array, callback, initialValue) {
-    return array.reduce(function(accumulator, currentValue) {
-        return callback(accumulator, currentValue); 
-    }, initialValue);
+    let accumulator = initialValue;
+    for (let i = 0; i < array.length; i++) {
+        accumulator = callback(accumulator, array[i]);
+    }
+    return accumulator;
 }
 
 function sumValues(accumulator, currentValue) {
-    return accumulator + currentValue; 
+    return accumulator + currentValue;
 }
 
 function execReduce() {
 
-    if(meuArrayEl.innerText)
-    {
-        
-        let value = document.getElementById("reduceInput").value; 
-    
+    let reduceInputEl = document.getElementById("reduceInput");
+    let inputUnshiftEl = document.getElementById("inputUnshift");
+
+    if (meuArrayEl.innerText) {
+
+        let value = reduceInputEl.value;
         let initialValue = parseInt(value) || 0;
-        
-        let reducedValue = reduce(array.map(Number), sumValues, initialValue); 
-        
-        document.getElementById("arrayReduce").innerText = reducedValue; 
+        let reducedValue = reduce(array.map(Number), sumValues, initialValue);
+        document.getElementById("arrayReduce").innerText = reducedValue;
 
     } else {
 
-        alert("O array está vazio!"); 
-        document.getElementById("inputUnshift").value = "";
+        alert("O array está vazio!");
+        inputUnshiftEl.value = "";
     }
-    
-
 }
+
